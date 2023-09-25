d34e63f8a51435034a1083cc222264c96d1fffe6
Xinyi Lu <xinyi.lu@airbnb.com>
2023-09-25 09:44:10 -0700

    add user/group info parameter for user to set ugi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='hmsclient',
-    version='0.1.1',
+    version='0.1.2',
 
     description='A package interact with the Hive metastore via the Thrift protocol',
     long_description=long_description,
